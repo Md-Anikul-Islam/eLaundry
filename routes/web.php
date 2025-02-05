d13446d42cbb20fc249018d320621c46ddb97c6f
Md-Anikul-Islam <anikul.islam01@northsouth.edu>
2025-02-05 17:27:56 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\admin\AdminDashboardController;
+use App\Http\Controllers\admin\SiteSettingController;
+use App\Http\Controllers\admin\SliderController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::middleware('auth')->group(callback: function () {
+    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+    Route::get('/unauthorized-action', [AdminDashboardController::class, 'unauthorized'])->name('unauthorized.action');
+
+    //Slider Section
+    Route::get('/slider-section', [SliderController::class, 'index'])->name('slider.section');
+    Route::post('/slider-store', [SliderController::class, 'store'])->name('slider.store');
+    Route::put('/slider-update/{id}', [SliderController::class, 'update'])->name('slider.update');
+    Route::get('/slider-delete/{id}', [SliderController::class, 'destroy'])->name('slider.destroy');
+
+    //Role and User Section
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+
+    //Site Setting
+    Route::get('/site-setting', [SiteSettingController::class, 'index'])->name('site.setting');
+    Route::post('/site-settings-store-update/{id?}', [SiteSettingController::class, 'createOrUpdate'])->name('site-settings.createOrUpdate');
+
+});
+
+require __DIR__.'/auth.php';
