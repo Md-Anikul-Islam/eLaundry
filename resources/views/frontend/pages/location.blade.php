bd48612505498e819dd23dc1968ea2525c86d591
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2025-02-25 18:21:03 +0600

    map updated

--- resources/views/frontend/pages/location.blade.php
+++ resources/views/frontend/pages/location.blade.php
@@ -73,6 +73,9 @@
                 </div>
             </div>
         </div>
+        <div class="contact-map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.5152933036306!2d-73.9287978235875!3d40.684647639289!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25c725b17c71b%3A0x728b4717055723e2!2s653%20Halsey%20St%2C%20Brooklyn%2C%20NY%2011233%2C%20USA!5e0!3m2!1sen!2sbd!4v1740485911605!5m2!1sen!2sbd" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
     </section>
     <!-- Contact section End -->
     
