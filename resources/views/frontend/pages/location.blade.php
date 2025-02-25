15e69c8542a804817375c862accf37a88cb4a114
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2025-02-25 11:31:31 +0600

    page header complete

--- resources/views/frontend/pages/location.blade.php
+++ resources/views/frontend/pages/location.blade.php
@@ -1,6 +1,27 @@
 @extends('frontend.layout')
 @section('content')
-    <h1 class="fs-1 text-center text-uppercase py-120 mt-5">this page under maintenance</h1>
+    <!-- Page Header Section Start-->
+    <section class="page-header-section section-bg">
+            <div class="container">
+                <div class="row g-4">
+                    <div class="col-md-5 align-self-center d-none d-md-block">
+                        <div class="header-img">
+                            <img src="{{URL::to('frontend/images/header/page_header_img.svg')}}" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-7 align-self-center">
+                        <div class="bread-crumb text-center text-md-end">
+                            <h1 class="page-header-title">Location</h1>
+                            <ul class="list-unstyled d-flex justify-content-center justify-content-md-end align-items-center">
+                                <li><a href="/">Home</a></li>
+                                <li>Location</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Page Header Section End-->
     <p class="fs-1 text-center text-uppercase py-120 mt-1">
         we are working on this page, please check back later
     </p>
